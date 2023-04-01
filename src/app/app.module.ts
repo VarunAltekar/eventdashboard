fc9bf8ebff66c92b55bc49d89a8207a4889f3637
VARUN <varunaltekar2@gmail.com>
2023-04-01 22:54:37 +0530

    adding new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './eventlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EventListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/eventlist.component.ts
@@ -0,0 +1,9 @@
+import { Component, NgModule } from "@angular/core";
+
+@Component({
+    selector: 'event-list',
+    template: `<div> <h1>Event List</h1> </div>`
+})
+export class EventListComponent{
+    
+}
