428e8caa5e6a49f2d2456df0a1b5f092e10b0a53
VARUN <varunaltekar2@gmail.com>
2023-04-01 23:17:35 +0530

    added template url

--- src/app/eventlist.component.ts
+++ src/app/eventlist.component.ts
@@ -2,8 +2,19 @@ import { Component, NgModule } from "@angular/core";
 
 @Component({
     selector: 'event-list',
-    template: `<div> <h1>Event List</h1> </div>`
+    /*
+    template: ` <div> 
+                    <h1>Event List</h1> 
+                    <h3>{{event.id}}</h3>
+                    <h3>{{event.name}}</h3>
+                </div>
+                `
+    */
+    templateUrl: 'eventlist.html'            
 })
 export class EventListComponent{
-    
+    event={
+        id:1,
+        name:'Varun'
+    }
 }
